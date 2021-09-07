230477798b0c515381d60020d6de0d4d12d32cf4
TomMc608 <tommc9010@gmail.com>
2021-09-07 16:40:23 +1200

    put all my javascript in to a .js file. All works well

--- WebSite/main.js
+++ WebSite/main.js
@@ -22,6 +22,49 @@ document.querySelector(".toggle").addEventListener("click",() => {
 
 })
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  //dots[slideIndex-1].className += " active";
+}
+
+var myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", "ed8fa7ffbf00e7ed51dca5edc25c11e9");
+myHeaders.append("x-rapidapi-host", "v1.hockey.api-sports.io");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+//look at the https://api-sports.io/documentation/hockey/v1#section/Architecture to find out how to write the URL
+fetch("https://v1.hockey.api-sports.io/teams/statistics?season=2019&team=29&league=3", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
 
 
 
