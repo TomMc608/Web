d2b1e282f033caf32955c7e4ac20c3e78a4bc92e
tommc608 <tommc9010@gmail.com>
2021-09-30 11:36:38 +1300

    1.2 port fixed for deployment maybe

--- WebSite/index.js
+++ WebSite/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 const expressLayouts = require('express-ejs-layouts')
 
 app.set("view engine","ejs");
@@ -15,7 +15,7 @@ app.get('/',(req, res) => {
 app.get('/home', (req, res) => res.render('index'))
 app.get('/user', (req, res) => res.render('Hello User'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 
 app.use(expressLayouts)
