7e916b9f834a28d0be563675350084704e62f170
tommc608 <tommc9010@gmail.com>
2021-11-16 13:12:19 +1300

    code cleaned up

--- WebSite/assets/java/api.js
+++ WebSite/assets/java/api.js
@@ -1,41 +1,24 @@
 //This java is for pulling api data and putting it in an id then displaying 
 //-it on the api webpage
-
 var myHeaders = new Headers();
 myHeaders.append("x-rapidapi-key", "ed8fa7ffbf00e7ed51dca5edc25c11e9");
 myHeaders.append("x-rapidapi-host", "v1.hockey.api-sports.io");
-
 //cant use var use let
 var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
+	method: 'GET',
+	headers: myHeaders,
+	redirect: 'follow'
 };
-
-
 //look at the https://api-sports.io/documentation/hockey/v1#section/Architecture to find out how to write the URL
-fetch("https://v1.hockey.api-sports.io/countries", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-    console.log(result);
-
-     console.log(result)
- 
-result.response.forEach(element => {
-
-    const CountryFlag = element.flag
-
-
-    let div = document.createElement("div");
-    let img = document.createElement("img");
-    img.src = CountryFlag;
-    div.append(img);
-    document.querySelector(".grid-container").append(div);
+fetch("https://v1.hockey.api-sports.io/countries", requestOptions).then(response => response.json()).then(result => {
+	console.log(result);
+	console.log(result)
+	result.response.forEach(element => {
+		const CountryFlag = element.flag
+		let div = document.createElement("div");
+		let img = document.createElement("img");
+		img.src = CountryFlag;
+		div.append(img);
+		document.querySelector(".grid-container").append(div);
+	});
 });
- 
-
-
-    
-  });
-
-
--- WebSite/assets/java/main.js
+++ WebSite/assets/java/main.js
@@ -1,77 +1,55 @@
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}  
+window.onbeforeunload = function() {
+	window.scrollTo(0, 0);
+}
 //This bit is for the boxes around the mouse
 let boxes = document.querySelectorAll('.box');
-    
-const onMouseMove = (e) =>{        
-  boxes.forEach((b,i) => {
-      setTimeout(()=>{   
-          b.style.opacity = 1 - (.1*i);
-          b.style.left = e.pageX + 'px';
-          b.style.top = e.pageY + 'px';           
-      },40*(i+5))      
-                                 
-  });
+const onMouseMove = (e) => {
+	boxes.forEach((b, i) => {
+		setTimeout(() => {
+			b.style.opacity = 1 - (.1 * i);
+			b.style.left = e.pageX + 'px';
+			b.style.top = e.pageY + 'px';
+		}, 40 * (i + 5))
+	});
 }
-
-
-
-
 document.addEventListener('mousemove', onMouseMove);
 console.log("box")
-
-document.querySelector(".toggle").addEventListener("click",() => {
-    document.querySelector(".toggle").classList.toggle("active");
-    document.querySelector(".menu").classList.toggle("active");
-    document.querySelector(".showcase").classList.toggle("active");
-
-
+document.querySelector(".toggle").addEventListener("click", () => {
+	document.querySelector(".toggle").classList.toggle("active");
+	document.querySelector(".menu").classList.toggle("active");
+	document.querySelector(".showcase").classList.toggle("active");
 })
-
 //-------------------------------------------------------------
 //This bit is for the slide show 
-
 var slideIndex = 0;
 showSlides();
 
 function plusSlides() {
-  showSlides();
+	showSlides();
 }
 
 function negSlides() {
-  slideIndex-=2;
-  showSlides();
+	slideIndex -= 2;
+	showSlides();
 }
-
-
 console.log("zzzzz")
 
 function showSlides() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  if (slideIndex <= 0) {slideIndex = 4}
-  x[slideIndex-1].style.display = "block";
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > x.length) {
+		slideIndex = 1
+	}
+	if (slideIndex <= 0) {
+		slideIndex = 4
+	}
+	x[slideIndex - 1].style.display = "block";
 }
-  setInterval(showSlides, 4000); // Change image every 2 seconds
-
-
-
-
-  //dots[slideIndex-1].className += " active";
-
-
+setInterval(showSlides, 4000); // Change image every 2 seconds
+//dots[slideIndex-1].className += " active";
 //-------------------------------------------------------------
 //This bit is for API
-
-
-  
-
-
-
-
