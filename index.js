a3f8e4ed26255c713c9a283828accbfe8b07188e
Tom <83677440+TomMc608@users.noreply.github.com>
2021-11-16 14:45:12 +1300

    layout.ejs added and works

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 const expressLayouts = require('express-ejs-layouts')
-
+app.use(expressLayouts)
 app.set("view engine","ejs");
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'assets')))
@@ -19,4 +19,3 @@ app.get('/API', (req, res) => res.render('API'))
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 
-app.use(expressLayouts)
